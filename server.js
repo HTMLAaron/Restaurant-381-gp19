29696509daba30e04775370ad6b26f733ea2242d
Aaron <43405823+HTMLAaron@users.noreply.github.com>
2019-12-01 00:25:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const qs = require ('querystring');
 const http = require('http');
 const url  = require('url');
 const MongoClient = require('mongodb').MongoClient;
@@ -8,25 +7,65 @@ const mongoDBurl = 'mongodb+srv://aaron:aaronso@aarondb-ep2mi.mongodb.net/test?r
 const dbName = 'AaronDB';
 var session = require('cookie-session');
 var express = require('express');
-app.set('trust poxy', 1);
+
 app = express();
-
-
-
 
 const server = http.createServer((req,res) => {
 	let timestamp = new Date().toISOString();
 	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
 
 	let parsedURL = url.parse(req.url,true); // true to get query as object 
-	let max = (parsedURL.query.max) ? parsedURL.query.max : 20;
-
-	
-   		 
-
+	let max = (parsedURL.query.max) ? parsedURL.query.max : 20;   		 
 
 	switch(parsedURL.pathname) {
-		
+		case '/':
+			res.writeHead(200,{"Content-Type": "text/html"});
+			res.write('<html><head>');
+			res.write('<title>Login</title>');
+			res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
+			res.write(' <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">');
+			res.write('</head><body>');
+        	res.write(' <header class="w3-container w3-teal">');
+			res.write('<h1>Login/Register</h1>');
+			res.write('</header>    ');     
+        	res.write(' <div class="w3-container w3-half w3-margin-top">');
+        	res.write(' <h3>Login</h3>');
+        	res.write(' <form action="/login" method="post" class="w3-container w3-card-2">');
+			res.write('	 <p>');
+			res.write(`	 <input name="logid" class="w3-input" type="text" style="width:90%" required="">`);
+			res.write('	 <label class="w3-label w3-validate">Name</label></p>');
+			res.write('	  <p>');
+			res.write(`	  <input name="password" class="w3-input" type="password" style="width:90%">`);
+			res.write('	 <label class="w3-label w3-validate">Password</label></p>');
+			res.write('	 <p>');
+			res.write(`	  <button class="w3-btn w3-section w3-teal w3-ripple"> Log in </button></p>`);
+			res.write('	 </form>	</div>   ');        
+            res.write('	<div class="w3-container w3-half w3-margin-top">');
+            res.write('      <h3>Register</h3>');
+            res.write('      <form action="/register" method="post" class="w3-container w3-card-2">');
+            res.write('         <p>');
+            res.write(`         <input name="regid" class="w3-input" type="text" style="width:90%" required="">`);
+            res.write('         <label class="w3-label w3-validate">Name</label></p>');
+            res.write('         <p>');
+            res.write(`         <input name="repassword" class="w3-input" type="password" style="width:90%">`);
+            res.write('         <label class="w3-label w3-validate">Password</label></p>');
+            res.write('         <p>');
+            res.write('         <p>');
+            res.write(`         <input name="confirmpassword" class="w3-input" type="password" style="width:90%">`);
+            res.write('         <label class="w3-label w3-validate">Confirm Password</label></p>');
+            res.write('         <p>   ');
+            res.write(`         <button class="w3-btn w3-section w3-teal w3-ripple"> Register </button></p>`);
+            res.write('      </form>');
+            res.write('</div> ');        
+			res.end('</body></html>	');
+			break;
+		case '/register':
+			login(res,parsedURL.query.criteria);
+			break;
+
+		case '/login':
+			
+
 
 		case '/read':
 			read_n_print(res,parseInt(max));
@@ -58,49 +97,48 @@ const server = http.createServer((req,res) => {
 			updateDoc(res,parsedURL.query);
 			break;
 		default:
-			login(parsedURL.query.criteria);
+			res.writeHead(404, {"Content-Type": "text/html"});
+			res.write('<html><body>');
+			res.write("404 Not Found\n");
+			res.end('<br><a href=/read?max=5>Give this a try instead?</a>');
+		
 	}
 });
 
 
-const login= (db,max,criteria,callback) =>{
-	app.post('/register', function(req,res){ 
-		var name = req.body.regid;
-		var passa = req.body.comfirmpassword; 
-		var passb = req.body.repassword; 
-		if (passa==passb) {  
-			var data = { 
-			"name": name,   
-			"password":passa
-			} 
 
-			db.collection('details').insertOne(data,function(err, collection){ 
-			if (err) throw err; 
-			console.log("New user inserted Successfully");                
-		   }); }
-		else { alert("The confirm password does not match."); return res.redirect('login.html'); }
-	  
-		return res.redirect('login.html'); 
-	 }) 
+const regsiter= (res,doc) => {
+	 
+	
+		let docObj = {};
+		try {
+			docObj = JSON.parse(doc);
+			//console.log(Object.keys(docObj).length);
+		} catch (err) {
+			console.log(`${doc} : Invalid document!`);
+		}
+		if (Object.keys(docObj).length > 0) {  // document has at least 1 name/value pair
+			const client = new MongoClient(mongoDBurl);
+			client.connect((err) => {
+				assert.equal(null,err);
+				console.log("Connected successfully to server");
+				const db = client.db(dbName);
+				db.collection('user').insertOne(docObj,(err,result) => {
+					assert.equal(err,null);
+					res.writeHead(200, {"Content-Type": "text/html"});
+					res.write('<html><body>');
+					res.write(`Inserted ${result.insertedCount} document(s) \n`);
+					res.end('<br><a href=/>Login</a>');					
+				});
+			});
+		} else {
+			res.writeHead(404, {"Content-Type": "text/html"});
+			res.write('<html>123<body>');
+			res.write(`${doc} : Invalid document!\n`);
+			res.end('<br><a href=/>Login</a>');	
+		}
+	
 
-		 app.post('/login', function(req,res){ 
-
-			var name = req.body.name; 
-			var pass = req.body.password; 
-		if (pass==pass)  {
-		   app.use(session({
-			  name: 'user',
-			  keys: []
-		   }));
-
-			db.collection('s381assignment').insertOne(data,function(err, collection){ 
-			if (err) throw err; 
-			console.log("New user inserted Successfully");                
-		   }); }
-		else { alert("The confirm password does not match."); return res.redirect('login.html'); }
-	  
-		return res.redirect('login.html'); 
-	 })
 
 }
 
@@ -263,3 +301,5 @@ const updateDoc = (res,newDoc) => {
 		res.end('<br><a href=/read?max=5>Home</a>');	
 	}
 }
+
+server.listen(process.env.PORT || 8099);
